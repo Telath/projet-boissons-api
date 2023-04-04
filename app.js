be017f2a98e8cb5da33d71075f61b6635beb1721
Clément Pouillart <84924577+Telath@users.noreply.github.com>
2023-04-04 14:38:02 +0200

    Lien avec la bdd

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose');
+
 var indexRouter = require('./routes/index');
 var boissonRouter = require('./routes/boisson');
 
 var app = express();
+
+// Intégration de la bdd
+var connectionString = "mongodb+srv://clementpouillart:PXetdikjakJh6Q4T@boissonsangular.bvhc3no.mongodb.net/test";
+var mongoDB = process.env.MONGODB_URI || connectionString;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use(logger('dev'));
 app.use(express.json());
