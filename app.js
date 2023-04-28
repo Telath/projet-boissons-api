dc02a5def3bcd92901c23d6dd97039ad340cb29f
Clément Pouillart <84924577+Telath@users.noreply.github.com>
2023-04-28 16:17:02 +0200

    Ajout de cors

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 
 var mongoose = require('mongoose');
 
+var cors = require('cors');
+
 var indexRouter = require('./routes/index');
 var boissonRouter = require('./routes/boisson');
 var bouteilleRouter = require('./routes/bouteille');
@@ -24,6 +26,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/boissons', boissonRouter);
